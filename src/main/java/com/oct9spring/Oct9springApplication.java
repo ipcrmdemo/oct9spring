6684d199575d73aec591fe80c7e880b74931dfec
Matt Cadorette <mcadorette@gmail.com>
2019-10-09 17:42:03 -0500

    Initial commit from Atomist
    [atomist:generated] [atomist:generator=create-spring]

---
+++ src/main/java/com/oct9spring/Oct9springApplication.java
@@ -0,0 +1,12 @@
+package com.oct9spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oct9springApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oct9springApplication.class, args);
+	}
+}
